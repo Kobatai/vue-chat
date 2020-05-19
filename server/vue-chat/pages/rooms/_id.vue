7a3a0074ca936f609b8e0eb17277ab67a2917d90
Kobatai <taiga1983346@gmail.com>
2020-05-19 22:44:30 +0900

    add: chat機能

--- server/vue-chat/pages/rooms/_id.vue
+++ server/vue-chat/pages/rooms/_id.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="relative">
-    <div class="px-4 pb-32"></div>
+    <div class="px-4 pb-4"></div>
+    <div v-for="chat in chats" :key="chat.id">
+      <!-- 自分の発言の場合 -->
+      <template v-if="isMyChat(chat.userId)">
+        <div class="flex my-4 justify-end">
+          <div class="bg-iceblue rounded-tr-md rounded-l-md p-4 mt-1 w-72">
+            <pre class="whitespace-pre-wrap text-sm">{{ chat.body }}</pre>
+          </div>
+        </div>
+      </template>
+      <!-- 相手の発言の場合 -->
+      <template v-else>
+        <div class="flex my-4">
+          <div class="mr-2 mt-2 min-w-3">
+            <img :src="chat.iconImageUrl" class="rounded-full h-10 w-10" />
+          </div>
+          <div class="w-72">
+            <span class="text-xs">{{ chat.name }}</span>
+            <div class="bg-gray-300 rounded-bl-md rounded-r-md p-4">
+              <pre class="whitespace-pre-wrap text-sm">{{ chat.body }}</pre>
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
     <form
       @submit.prevent="onSubmit"
       class="fixed bottom-0 bg-white w-full max-w-sm flex py-4 border-t border-gray-300"
@@ -21,7 +45,7 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapGetters, mapMutations } from "vuex";
 export default {
   middleware: ["checkAuth"],
   data() {
@@ -30,14 +54,37 @@ export default {
         message: {
           val: null
         }
-      }
+      },
+      unsubscribe: null
     };
   },
 
   computed: {
+    ...mapGetters("chats", ["chats"]),
     isValidateError() {
       return !this.form.message.val;
     }
+  },
+
+  // asyncDataはページコンポーネントの初期化前に実行される関数　コンポーネントへデータをセットすることを目的として使用
+  // fetchもページコンポーネントの初期化前に実行される関数　Vuex storeにデータを格納することを目的として使用するといった違いがある
+  // paramsでpathのパラメータを取得
+  async asyncData({ store, params }) {
+    const roomId = params.id;
+    const unsubscribe = await store.dispatch("chats/subscribe", {
+      roomId
+    });
+
+    return {
+      unsubscribe
+    };
+  },
+
+  // コンポーネントが破棄される段階でState の中を空にし、リアルタイムアップデートを停止
+  destroyed() {
+    // this.$store.dispatch("chats/clear");
+    this.$store.commit("chats/clear");
+    if (this.unsubscribe) this.unsubscribe();
   },
 
   methods: {
@@ -86,6 +133,10 @@ export default {
 
     resetForm() {
       this.form.message.val = null;
+    },
+    isMyChat(userId) {
+      const { uid } = this.$fireAuth.currentUser;
+      return userId === uid;
     }
   }
 };
